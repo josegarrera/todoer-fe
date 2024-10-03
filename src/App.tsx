aeb51ce2a3ad63710066b9a06c94e730c353bf44
Jose Garrera <josegarrera@gmail.com>
2024-10-03 15:00:01 -0300

    feat: first implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo {
+  _id: string;
+  title: string;
+  completed: boolean;
 }
 
-export default App
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await axios.get('http://localhost:3001/todos');
+    setTodos(response.data);
+  };
+
+  const addTodo = async () => {
+    const response = await axios.post('http://localhost:3001/todos', {
+      title,
+    });
+    setTodos([...todos, response.data]);
+    setTitle('');
+  };
+
+  const deleteTodo = async (_id: string) => {
+    await axios.delete(`http://localhost:3001/todos/${_id}`);
+    setTodos(todos.filter(todo => todo._id !== _id));
+  };
+
+  return (
+    <div>
+      <h1>TODO List</h1>
+      <input
+        type="text"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        placeholder="New Todo"
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.title}
+            <button onClick={() => deleteTodo(todo._id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
